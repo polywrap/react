94ec26b9b48cee9befdc1bf47ff264fe26dbab50
Cesar <cesarbrazon10@gmail.com>
2023-03-07 23:31:07 +0100

    chore: add peer dependencies & remove not needed config

--- src/__tests__/config.ts
+++ src/__tests__/config.ts
@@ -1,11 +1,10 @@
-import { plugin as ensResolverPlugin } from "@polywrap/ens-resolver-plugin-js";
 import {
   Connection,
   Connections,
   ethereumProviderPlugin,
 } from "@polywrap/ethereum-provider-js";
-import { ClientConfigBuilder, defaultIpfsProviders, IClientConfigBuilder } from "@polywrap/client-config-builder-js";
-import { ensAddresses, providers } from "@polywrap/test-env-js";
+import { ClientConfigBuilder, IClientConfigBuilder } from "@polywrap/client-config-builder-js";
+import { providers } from "@polywrap/test-env-js";
 
 export function getClientConfig() {
   const builder = configure(new ClientConfigBuilder())
@@ -28,17 +27,7 @@ export function configure(builder: IClientConfigBuilder): IClientConfigBuilder {
           }
         }),
       }),
-      "wrap://ens/ens-resolver.polywrap.eth": ensResolverPlugin({
-        addresses: {
-          testnet: ensAddresses.ensAddress
-        },
-      }),
     })
-    .addEnv("wrap://package/ipfs-resolver", {
-        provider: providers.ipfs,
-        fallbackProviders: defaultIpfsProviders,
-      }
-    )
     .addInterfaceImplementation(
       "wrap://ens/wraps.eth:ethereum-provider@1.1.0",
       "wrap://ens/wraps.eth:ethereum-provider@1.1.0"
